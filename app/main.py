2ce5c3375b6f6f97d3321e758598d5d0fa51a36a
George <georgios.kyziridis@devoteam.com>
2024-04-25 11:45:35 +0200

    testing

--- app/main.py
+++ app/main.py
@@ -161,3 +161,6 @@ if __name__ == "__main__":
     # LOGGER.debug("Launching server...")
     # app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
     app.run(debug=True, port=8080, host="0.0.0.0")
+
+# europe-west4-docker.pkg.dev/qwiklabs-gcp-03-c86c31d83de6/sensire-docker
+# gcloud builds submit --region=europe-west4 --tag europe-west4-docker.pkg.dev/qwiklabs-gcp-03-c86c31d83de6/sensire-docker:latest .
